045710e233e144454d1af19fd54e6ba86c3bf141
Debankur04 <debankurdutta04@gmail.com>
2026-05-01 19:50:20 +0530

    Added minor changes

---
+++ agent_file/utils/railway_utils.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+class Railway_tool:
+    def __init__(self):
+        X-API-Key = os.getenv('RAILWAY_RADAR_API_KEY')
+        pass
+
+    
